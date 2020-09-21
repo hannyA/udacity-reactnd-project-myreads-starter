d2c1ac657bf7ea3c1641a8542ab988cb1fff4afe
hannya <hanny.aly@gmail.com>
2020-09-21 04:43:06 +0000

    refactor: Bookshelf is a stateless functional component

--- src/BookShelf.js
+++ src/BookShelf.js
@@ -1,24 +1,23 @@
-import React, {Component} from 'react'
+import React from 'react'
 import BookItem from './BookItem'
 
 
-class BookShelf extends Component {
+const BookShelf = (props) => {
 	  
-  render() {
-    const {books, shelfName } = this.props    
-    return (
-      <div className="bookshelf">
-        <h2 className="bookshelf-title">{shelfName}</h2>
-        <div className="bookshelf-books">
-          <ol className="books-grid">
-            {books.map((book) => (
-              <BookItem book={book} key={book.id} onMovedShelf={this.props.onMovedShelf}/>
-            ))}
-          </ol>
-        </div>
+  const {books, shelfName, onMovedShelf } = props  
+    
+  return (
+    <div className="bookshelf">
+      <h2 className="bookshelf-title">{shelfName}</h2>
+      <div className="bookshelf-books">
+        <ol className="books-grid">
+          {books.map((book) => (
+            <BookItem book={book} key={book.id} onMovedShelf={onMovedShelf}/>
+          ))}
+        </ol>
       </div>
-    )
-  }
+    </div>
+  )
 }
 
 export default BookShelf
