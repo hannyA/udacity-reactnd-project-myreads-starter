efe5bcbb036fe3462b2bbfe840f71edfa4c38c21
root <hanny.aly@gmail.com>
2020-09-22 04:28:07 +0000

    feat: Add PropTypes

--- src/SearchPage.js
+++ src/SearchPage.js
@@ -2,8 +2,7 @@ import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 import BookItem from './BookItem'
 import * as BooksAPI from './BooksAPI'
-
-
+import PropTypes from 'prop-types'
 
 class SearchPage extends Component {
   
@@ -36,29 +35,29 @@ class SearchPage extends Component {
         if (this.state.query === query) {
           if (Array.isArray(searchResults)) {			
             const filteredBooks = searchResults.filter((book) => (
-                	book.hasOwnProperty('imageLinks') && book['imageLinks'].hasOwnProperty('thumbnail')
-            	)).map((resultBook) => {
-	              
-          		  const ownedBook = this.props.books.filter((book) => book.id === resultBook.id)
-            
-		          if (ownedBook.length > 0) {
-                    resultBook.shelf = ownedBook[0].shelf
-                  } else {
-                  	resultBook.shelf = 'none'
-                  }
-                  return resultBook
-            	})
+              book.hasOwnProperty('imageLinks') && book['imageLinks'].hasOwnProperty('thumbnail')
+            )).map((resultBook) => {
+
+              const ownedBook = this.props.books.filter((book) => book.id === resultBook.id)
+
+              if (ownedBook.length > 0) {
+                resultBook.shelf = ownedBook[0].shelf
+              } else {
+                resultBook.shelf = 'none'
+              }
+              return resultBook
+            })
             this.setState(() => ({
                 searchResults: filteredBooks
             }))  
 
           } else { // Results is object with error message
-              console.log("searchResults is not array " )
+            console.log("searchResults is not array " )
 
-              this.setState(() => ({
-                searchResults: []
-              }))   
-          }   
+            this.setState(() => ({
+              searchResults: []
+            }))   
+          }
         }                  
       })
     }
@@ -82,7 +81,6 @@ class SearchPage extends Component {
               value={this.state.query}
               onChange={(e)=> this.updateQuery(e.target.value)}
             />
-
           </div>
         </div>
         <div className="search-books-results">
@@ -94,6 +92,11 @@ class SearchPage extends Component {
         </div>
       </div>
 	)}
+}
+
+SearchPage.propTypes = {
+  updateShelf: PropTypes.string.isRequired,
+  books: PropTypes.array.isRequired
 }
 
 export default SearchPage
